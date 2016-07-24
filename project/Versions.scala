e3e99e1249c2cf273cb07d6b04ef66646882f4d2
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-07-24 20:33:59 +0000

    IMCEI-272 - Add connector to dump OTI MOF profiles/models for SPARK dependencies update

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
 
   // this project's version
-  val version = "2.3.2"
+  val version = "2.4.0"
 
   // scalaLibs
 
@@ -10,11 +10,19 @@ object Versions {
   val scala_version = "2.11.7"
 
   // Scala library modules
-  val scala_xml_version = "1.0.4"
+
+  // http://mvnrepository.com/artifact/org.scala-lang.modules/scala-xml_2.11
+  val scala_xml_version = "1.0.5"
+
+  // http://mvnrepository.com/artifact/org.scala-lang.modules/scala-parser-combinators_2.11
   val scala_parser_combinators_version = "1.0.4"
+
+  //http://mvnrepository.com/artifact/org.scala-lang.modules/scala-swing_2.11
   val scala_swing_version = "1.0.2"
 
   // Scala compiler plugins
+
+  // http://mvnrepository.com/artifact/org.scala-lang.plugins/scala-continuations-library_2.11
   val scala_continuations_version = "1.0.2"
 
 }
